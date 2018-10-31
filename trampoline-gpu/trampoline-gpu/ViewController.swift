3fd6d9f4266852fe7f75fbcef07927f825aaa775
Oliver Portee <oliver.portee@gmail.com>
2018-10-31 21:14:36 +0100

    added two empty xcode projects

---
+++ trampoline-gpu/trampoline-gpu/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  trampoline-gpu
+//
+//  Created by Oliver Portee on 31.10.18.
+//  Copyright © 2018 Oliver Portee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
+
